4b39aed00054dcb1e582cdb05d7e83542e303832
Moin Haidar <moinhaidar@gmail.com>
2012-01-09 12:18:16 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moinhaidar::Application.config.secret_token = '577c1ac63416f8c42273a7781693ebe05f738af8715616e10959de39f1b29b6db281b747493188eaf929c98f7033fa6043511a3953259a04ee8a196ef57f603b'
